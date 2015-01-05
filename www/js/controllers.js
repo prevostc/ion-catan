a974e5ed2023aa32ddec3476a212aca69e939809
Clément Prévost <prevostclement@gmail.com>
2015-01-05 22:55:46 +0100

    identify ugly code with todoz

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,6 +4,8 @@ angular.module('starter.controllers', [])
     $scope.generate = function() {
         Catan.generateMap(document.querySelector('.canvas'), Settings.getTileTrioScoreLimit());
     };
+    // @todo: find another solution. This code wait for the interface to be loaded so that the "generate"
+    // button and bottom bar are loaded and so the canvas-container gets his final height
     setTimeout(function(){
         var canvas = document.querySelector('.canvas');
         canvas.width = document.querySelector('.canvas-container').offsetWidth;
